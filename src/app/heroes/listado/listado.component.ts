fad8bf5697c69723ec98f4899e87d11bc2ac12f8
Enrique Moreno AW <emove17@gmail.com>
2021-09-16 19:53:25 -0600

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Hulk','Iron Man','Thor','Spier-Man','Dr. Strange', 'Capitan America'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    
+    //this.heroes.splice(this.heroes.length - 1, this.heroes.length)
+     this.heroeBorrado = this.heroes.shift() || '';
+    
+  }
+
+}
